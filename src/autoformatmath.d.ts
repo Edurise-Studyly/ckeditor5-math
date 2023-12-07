811a980b8886e6afdc32c608139cec00156a3b9e
Filip Povolny <filip.povolny@studyly.com>
2023-12-07 01:05:24 +0100

    remove declaration files

--- src/autoformatmath.d.ts
+++
@@ -1,5 +0,0 @@
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-
-declare module 'ckeditor5-math/src/autoformatmath' {
-    export default class AutoformatMath extends Autoformat {}
-}
